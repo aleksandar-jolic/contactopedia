89319a147b648ddf1f2e991c586c15cc3271b871
Aleksandar Jolic <coajolic@gmail.com>
2026-03-30 13:38:59 +0200

    First commit

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <div className="text-center mt-3 border-top text-muted py2">
+            <span className="text-white-50">Copyright &copy; 2024</span>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/layout/Homepage.jsx
@@ -0,0 +1,13 @@
+import Header from './layout/Header'
+import Footer from './layout/Footer';
+
+function HomePage(){
+    return(
+        <div>
+            <Header />
+            <Footer />  
+        </div>
+    )
+}
+
+export default HomePage;
